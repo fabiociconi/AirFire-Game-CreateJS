7ec1e6f0f7ec809658ed51a6cd68d52fd141056a
ZeusAC <fabiociconi@yahoo.com.br>
2017-08-05 01:50:48 -0300

    cad

---
+++ AirFire/js/data.js
@@ -0,0 +1,15 @@
+ var data = {
+        //"images": ["images/spritesheet.png"],
+
+            "frames": [
+                    [768,512,256,256],
+                    [1280,512,256,256]],
+                   
+        "animations": {
+            "playButton":{frames:[0]},
+            "playButton2":[2]
+                
+        }
+    };
+
+   
--- AirFire/js/scenes/GameMenu.js
+++ AirFire/js/scenes/GameMenu.js
@@ -12,20 +12,67 @@
 
     p.titleTxt = null;
     p.count = 0;
-
+//fabio
+         //var spritesheet = new createjs.SpriteSheet('images/spritesheet.png');
+       
+         var data1 = {
+        "images": ["images/spritesheet.png"],
+            "frames": [
+                   [768,512,256,256],
+                    [1280,512,256,256]],
+                   //3328,"y":512,"w":256,"h":256 
+                //[768,512,256,256]
+                 //   [1280,512,256,256] ],                   
+        "animations": {
+            "but":{frames:[0,1],"speed":2.1}    
+            //"but2":{frames:[1],"speed":2.1}
+            //"playButton2":[2]
+            //"x":0,"y":2176,"w":640,"h":640
+        }
+    };
+     var data = 
+        {
+          
+            "images": ["images/images.png"],
+            "frames": [
+                [0, 0, 512, 512],
+                [512, 0, 512, 512],
+                [1024, 0, 512, 512],
+                [1536, 0, 512, 512],
+                [0, 512, 256, 256],
+                [256, 512, 256, 256],
+                [512, 512, 256, 256],
+                [768, 512, 256, 256],
+                [1024, 512, 256, 256],
+                [1280, 512, 256, 256],
+                [1536, 512, 256, 256],
+                [1792, 512, 256, 256]],
+            "animations": 
+            {
+                "airship":{frames:[0,1,2,3],"speed":2.1},
+                "playButton": {frames: [9,10],"speed":2.1},
+                "creditsButton": { frames: [4,5],"speed":2.1 },
+                "exitButton": { frames: [6, 7],"speed":2.1 }
+                
+            }
+        
+    };
+ //fabio
     p.initialize = function () {
         this.Container_initialize();
         this.addBG();
         this.addTitle();
         //this.addOrbs();
-        this.addButton();
+        //this.addButton();
+        //fabio menu buttons
+        this.addButtons()
+        //fabio menu buttons
        
     }
     p.addBG = function () {
 
         var imgBackground = 'images/milkyway.jpg';
-
-
+        
         var background2 = new createjs.Bitmap(imgBackground);
         background2.scaleX = background2.scaleY = 0.2;
         background2.x = background2.y = 0;
@@ -65,6 +112,38 @@
         btn.y = 400;
         btn.setButton({upColor:'FF0000', color:'#FFF', borderColor:'#F00', overColor:'#900'});
         this.addChild(btn);
+    }
+     p.addButtons = function () {
+        var bounds;
+        var yPos = 850;
+        
+        //this.playButton = new createjs.Sprite(data, "airship");
+        // bounds = this.playButton.getBounds();
+        // this.playButton.regX = bounds.width / 2;
+        // this.playButton.x = screen_width / 2;
+        // this.playButton.y = yPos;
+        // this.contBtn = this.playButton.clone();
+        // this.contBtn.gotoAndStop("playButton2");
+        // this.contBtn.y = (yPos + bounds.height * 1.5);
+        //this.gameBtn.on('click', this.onButtonClick, this);
+       // this.contBtn.on('click', this.onButtonClick, this);
+        //this.addChild(this.gameBtn, this.contBtn);
+        //this.addChild(this.playButton);
+        var ss = new createjs.SpriteSheet(data1);
+        var playButton = new createjs.Sprite(ss,'but'); 
+        //bounds = playButton.getBounds();
+        playButton.on('click',this.playGame,this);
+        playButton.regX = playButton.width / 2;
+        playButton.scaleX =0.5;
+        playButton.scaleY =0.5;
+        playButton.x = 550;
+        playButton.y = 280;
+        //airplane.gotoAndStop("but") ;
+        //airplane.x = 800 / 2;
+        //airplane = yPos;
+
+        //airplane.gotoAndStop("but2");
+          this.addChild(playButton);
     }
     p.playGame = function (e) {
         this.dispatchEvent(game.GameStateEvents.GAME);
