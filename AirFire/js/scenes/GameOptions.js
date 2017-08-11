a0f5e3c5ca5418aef79b92b1cd02e4b34b2f23d0
lemosandre <lemosandrecosta@gmail.com>
2017-08-11 13:24:36 -0400

    Opition , as images ainda nao tao legal

--- AirFire/js/scenes/GameOptions.js
+++ AirFire/js/scenes/GameOptions.js
@@ -19,32 +19,60 @@
         this.addBG();
         this.addTitle();
         this.addButton();
+        this.addImageArrow();
+        this.addImageSpace();
     }
     p.addTitle = function () {
         this.titleTxt = new createjs.Text("OPTIONS!", 'bold 40px Cambria', '#F00');
         this.titleTxt.x = canvas.width / 2;
-        this.titleTxt.y = 150;
+        this.titleTxt.y = 100;
         this.titleTxt.textAlign = 'center';
+        this.titleTxt1 = new createjs.Text("CONTROL!", 'bold 40px Cambria', '#F00');
+        this.titleTxt1.x = canvas.width / 2;
+        this.titleTxt1.y = 150;
+        this.titleTxt1.textAlign = 'center';
+
+        this.titleTxt2 = new createjs.Text("Space ship control!", 'bold 20px Cambria', '#F00');
+        this.titleTxt2.x = 400;
+        this.titleTxt2.y = 400;
+        this.titleTxt2.textAlign = 'center';
+
+        this.titleTxt3 = new createjs.Text("Space ship shoot control!", 'bold 20px Cambria', '#F00');
+        this.titleTxt3.x = 750;
+        this.titleTxt3.y = 400;
+        this.titleTxt3.textAlign = 'center';
        // this.addChild(this.titleTxt);
 
-        this.titleTxt1 = new createjs.Text("Andre Lemos\n" +
-                                           "Fabio Ciconi\n" +
-                                           "Rodrigo Geronimo\n"+
-                                           "\n"+
-                                           "\nThanks to: "+
-                                           "Sprites: http://unluckystudio.com/", 
-                                           '40px Cambria', 
-                                           'yellow');
-        this.titleTxt1.x = canvas.width / 2;;
-        this.titleTxt1.y = canvas.height / 2 - 50;
-        this.titleTxt1.textAlign = 'center';
+      
        
-        this.addChild(this.titleTxt1,this.titleTxt);
+       
+        this.addChild(this.titleTxt, this.titleTxt1,this.titleTxt2,this.titleTxt3);
         // this.alpha = 0;
         //createjs.Tween.get(this.titleTxt1).to({ alpha: 1 }, 1000);
+    }
+
+    p.addImageArrow = function () {
+        var img = 'assets/arrow.png';
+        var img2 = new createjs.Bitmap(img);
 
 
+        img2.scaleX = img2.scaleY = 1;
+        img2.x = 150;
+        img2.y = 60;
 
+        this.addChild(img2);
+    }
+
+    p.addImageSpace = function () {
+        var imgBar = 'assets/spacebar.png';
+        var imgBar2 = new createjs.Bitmap(imgBar);
+
+
+        imgBar2.scaleX = imgBar2.scaleY = 1.50;
+        imgBar2.x = 550;
+        imgBar2.y = 300;
+
+        this.addChild(imgBar2);
     }
 
     p.addBG = function () {
@@ -62,6 +90,7 @@
 
         this.addChild(bg);
     }
+
     p.addButton = function () {
         var btn, event;
         btn = new ui.SimpleButton('BACK');
